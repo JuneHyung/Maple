f119a74b3537935a954c54246b60d77d192f5452
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2024-01-09 14:02:38 +0900

    faet: project init

---
+++ frontend/src/components/TestComponent.tsx
@@ -0,0 +1,7 @@
+const TestComponent = () => {
+  return (
+    <div>Test Component</div>
+  )
+}
+
+export default TestComponent;
---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,12 @@
+import './../assets/scss/App.scss';
+import TestComponent from '../components/TestComponent';
+
+const App = () => {
+  return (
+    <div className="App">
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
